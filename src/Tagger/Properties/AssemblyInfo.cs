a12e2487e99e7ebeb3f8706ad6af0abce6bcee1f
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-07-12 15:13:56 +0200

    Updating AssemblyInfo(s)

--- src/Tagger/Properties/AssemblyInfo.cs
+++ src/Tagger/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Tagger")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright (c) 2015 Giacomo Stelluti Scala")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
