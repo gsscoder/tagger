face7a91c5bc576e6149b8872e2e885674351389
Giacomo Stelluti Scala <gsscoder@gmail.com>
2019-12-19 20:45:20 +0100

    Used nameof() in ForEach<T>()

--- src/Tagger/Infrastructure/EnumerableExtensions.cs
+++ src/Tagger/Infrastructure/EnumerableExtensions.cs
@@ -7,11 +7,10 @@ namespace Tagger.Infrastructure
     {
         public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
         {
-            if (source == null) throw new ArgumentNullException("source");
-            if (action == null) throw new ArgumentNullException("action");
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (action == null) throw new ArgumentNullException(nameof(action));
 
-            foreach (var element in source)
-            {
+            foreach (var element in source) {
                 action(element);
             }
         }
