e18f84f55339fc448e0d69ce5a729d586afe245d
Giacomo Stelluti Scala <gsscoder@gmail.com>
2020-01-18 07:13:10 +0100

    Renamed an internal test class

---
+++ tests/Tagger.Reflect.Tests/TypeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+static class TypeExtensions
+{
+    public static IEnumerable<object> AllAttributes<T>(this Type type, string propertyName)
+        where T : Attribute
+    {
+        var prop = type.GetProperties().Single(p => p.Name.Equals(propertyName));
+        return prop.GetCustomAttributes(typeof(T), true);
+    }
+
+    public static T SingleAttribute<T>(this Type type, string propertyName)
+        where T : Attribute
+    {
+        return (T)type.AllAttributes<T>(propertyName).Single();
+    }
+}
