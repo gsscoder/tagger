d7c7fb98adbf0032bcd9e8cd51d8716f14e5cd2c
Giacomo Stelluti Scala <gsscoder@gmail.com>
2020-02-01 09:37:25 +0100

    Used member expr in MetaDataExtensions class

--- src/Tagger.Reflect/MetadataExtensions.cs
+++ src/Tagger.Reflect/MetadataExtensions.cs
@@ -3,21 +3,17 @@ using System.Collections.Generic;
 
 static class MetadataExtensions
 {
-    public static Metadata WithInterfaces(this Metadata metadata, IEnumerable<Type> interfaces)
-    {
-        return new Metadata(
+    public static Metadata WithInterfaces(this Metadata metadata, IEnumerable<Type> interfaces) =>
+        new Metadata(
             metadata.Template,
             metadata.Attributes,
             metadata.Properties,
             interfaces);
-    }
 
-    public static Metadata WithProperties(this Metadata metadata, IEnumerable<PropertyMeta> properties)
-    {
-        return new Metadata(
+    public static Metadata WithProperties(this Metadata metadata, IEnumerable<PropertyMeta> properties) =>
+         new Metadata(
             metadata.Template,
             metadata.Attributes,
             properties,
             metadata.Interfaces);
-    }
 }
